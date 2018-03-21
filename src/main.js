2d42cf8f78123e1f3aafc9eaf86a992c43a6b227
Guest User <Guest@epicodus-4.local>
2018-03-21 15:26:45 -0700

    update how output was being displayed

--- src/main.js
+++ src/main.js
@@ -26,10 +26,7 @@ $(document).ready(function() {
       for(let i = 0; i < response.bikes.length; i++){
         // if(response.bikes[i].stolen_location == location && response.bikes[i].frame_colors == color && response.bikes[i].manufacturer_name == manufacture &&  response.bikes[i].date_stolen== date)
         // {
-          $('#location').append(response.bikes[i].stolen_location);
-          $('#color').append(response.bikes[i].frame_colors);
-          $('#manufacture').append(response.bikes[i].manufacturer_name);
-          $('#date').append(response.bikes[i].date_stolen);
+         $('#bike-requested-info').append("<strong>Location:</strong> " + response.bikes[i].stolen_location  + '<br>' + "<strong>Color:</strong> " + response.bikes[i].frame_colors +'<br>' + "<strong>Manufacture:</strong> " +  response.bikes[i].manufacturer_name + '<br>'+ "<strong>Date Stolen:</strong> " + Date(response.bikes[i].date_stolen) + '<hr>');
 
           $("#bike-requested-info").show();
         // }
