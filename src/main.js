26b18f24b43500ae99a551ed4e595568945916c6
Guest User <Guest@epicodus-4.local>
2018-03-21 16:41:28 -0700

    added show hide functionality

--- src/main.js
+++ src/main.js
@@ -26,9 +26,11 @@ $(document).ready(function() {
       for(let i = 0; i < response.bikes.length; i++){
         // if(response.bikes[i].stolen_location == location && response.bikes[i].frame_colors == color && response.bikes[i].manufacturer_name == manufacture &&  response.bikes[i].date_stolen== date)
         // {
-         $('#bike-requested-info').append(`<img src=${response.bikes[i].thumb}>`+"<br><strong>Location:</strong> " +  response.bikes[i].stolen_location  + '<br>' + "<strong>Color:</strong> " + response.bikes[i].frame_colors +'<br>' + "<strong>Manufacture:</strong> " +  response.bikes[i].manufacturer_name + '<br>'+ "<strong>Date Stolen:</strong> " + Date(response.bikes[i].date_stolen)  + '<hr>');
+         $('#bike-results').append(`<img src=${response.bikes[i].thumb}>`+"<br><strong>Location:</strong> " +  response.bikes[i].stolen_location  + '<br>' + "<strong>Color:</strong> " + response.bikes[i].frame_colors +'<br>' + "<strong>Manufacture:</strong> " +  response.bikes[i].manufacturer_name + '<br>'+ "<strong>Date Stolen:</strong> " + Date(response.bikes[i].date_stolen)  + '<hr>');
 
-          $("#bike-requested-info").show();
+          $("#bike-requested-info").hide();
+          $("#bike-results").show();
+          $("#bike").show();
         // }
       }
     })
