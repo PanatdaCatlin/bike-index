4b8bb135dcbc6392c87b825384d13243b2ebe46b
Guest User <Guest@epicodus-4.local>
2018-03-21 14:49:59 -0700

    incorporate API to display information on screen

--- src/main.js
+++ src/main.js
@@ -3,53 +3,40 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import './../.env';
 import $ from 'jquery';
+import { Bike } from './../js/Bike.js';
 
 $(document).ready(function() {
 
-      $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=&distance=10&stolenness=proximity&access_token=` + process.nev.API_KEY).then(function(response) {
-        console.log(response);
+  $("#bike-info").submit(function(event){
+    event.preventDefault();
 
-        $('#location').text(response.main.location);
+    let location = $("#stolen-location").val();
+    let color = $("#bike-color").val();
+    let manufacture = $("#bike-manufacture").val();
+    let date = $("#bike-date").val();
 
-      }).fail(function(error) {
-        $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
-      });
+    $('#stolen-location').val("");
+    $("#bike-color").val("");
+    $("#bike-manufacture").val("");
+    $("#bike-date").val("");
 
-      $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=&distance=10&stolenness=proximity&access_token=` + process.nev.API_KEY).then(function(response) {
-        console.log(response);
+    $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=10&stolenness=proximity&access_token=process.env.API_KEY`).then(function(response) {
+      console.log(response);
 
-        $('#color').text(response.main.color);
+      for(let i = 0; i < response.bikes.length; i++){
+        // if(response.bikes[i].stolen_location == location && response.bikes[i].frame_colors == color && response.bikes[i].manufacturer_name == manufacture &&  response.bikes[i].date_stolen== date)
+        // {
+          $('#location').append(response.bikes[i].stolen_location);
+          $('#color').append(response.bikes[i].frame_colors);
+          $('#manufacture').append(response.bikes[i].manufacturer_name);
+          $('#date').append(response.bikes[i].date_stolen);
 
-      }).fail(function(error) {
-        $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
-      });
-
-      $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=&distance=10&stolenness=proximity&access_token=` + process.nev.API_KEY).then(function(response) {
-        console.log(response);
-
-        $('#manufacture').text(response.main.manufacture);
-
-      }).fail(function(error) {
-        $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
-      });
-
-      $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=&distance=10&stolenness=proximity&access_token=` + process.nev.API_KEY).then(function(response) {
-        console.log(response);
-
-        $('#date').text(response.main.date);
-
-      }).fail(function(error) {
-        $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
-      });
-      $("#bike-info").submit(function(event){
-        event.preventDefault();
-
-        let location = $("#bike-location").val());
-       let color = $("#bike-color").val();
-       let manufacture = $("#bike-manufacture").val();
-       let date = $("#bike-date").val();
-
-       $("#bike-requested-info").show();
-     })
+          $("#bike-requested-info").show();
+        // }
+      }
     })
-    });
+    .fail(function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+    })
+  })
+});
