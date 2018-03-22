078c80d6a098c4b060a87cbe9a048272bcde5a48
Jamie Pittak <charrawr456@gmail.com>
2018-03-21 20:29:38 -0700

    adds search by color functionality

--- src/main.js
+++ src/main.js
@@ -20,18 +20,24 @@ $(document).ready(function() {
     $("#bike-manufacture").val("");
     $("#bike-date").val("");
 
-    $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=${manufacture}&location=${location}&distance=10&stolenness=proximity&access_token=process.env.API_KEY`).then(function(response) {
+    $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=100&manufacturer=${manufacture}&location=${location}&distance=10&stolenness=proximity&access_token=process.env.API_KEY`).then(function(response) {
       console.log(response);
 
       for(let i = 0; i < response.bikes.length; i++){
-        // if(response.bikes[i].stolen_location == location && response.bikes[i].frame_colors == color && response.bikes[i].manufacturer_name == manufacture &&  response.bikes[i].date_stolen== date)
-        // {
-         $('#bike-results').append(`<img src=${response.bikes[i].thumb}>`+"<br><strong>Location:</strong> " +  response.bikes[i].stolen_location  + '<br>' + "<strong>Color:</strong> " + response.bikes[i].frame_colors +'<br>' + "<strong>Manufacture:</strong> " +  response.bikes[i].manufacturer_name + '<br>'+ "<strong>Date Stolen:</strong> " + Date(response.bikes[i].date_stolen)  + '<hr>');
+        console.log(response.bikes[i].stolen_location);
+        for(let j = 0; j < response.bikes[i].frame_colors.length; j++){
+          console.log(response.bikes[i].frame_colors);
+          if(color === response.bikes[i].frame_colors[j] || color === ""){
+            console.log(color);
 
-          $("#bike-requested-info").hide();
-          $("#bike-results").show();
-          $("#bike").show();
-        // }
+            $('#bike-results').append(`<img src=${response.bikes[i].thumb}>`+"<br><strong>Location:</strong> " +  response.bikes[i].stolen_location  + '<br>' + "<strong>Color:</strong> " + response.bikes[i].frame_colors +'<br>' + "<strong>Manufacture:</strong> " +  response.bikes[i].manufacturer_name + '<br>'+ "<strong>Date Stolen:</strong> " + Date(response.bikes[i].date_stolen)  + '<hr>');
+
+            $("#bike-requested-info").hide();
+            $("#bike-results").show();
+            $("#bike").show();
+          }
+        }
+
       }
     })
     .fail(function(error) {
